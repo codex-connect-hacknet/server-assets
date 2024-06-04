95456480825e8dcabf699b9fe55c50f127cfbfa0
Tanjil <tanjilkh@gmail.com>
2024-06-04 21:24:29 +0100

    create server

---
+++ config/ssh_conn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER="admin"
+HOSTNAME="server-codex-connect-assets"
+IP="211.117.107.115"
+PORT="2222"
+KEY="noonewillgessmypassword:D"
+
+echo "Connecting to $USER@$HOSTNAME on port $PORT..."
+ssh -i $KEY $USER@$HOSTNAME -p $PORT
